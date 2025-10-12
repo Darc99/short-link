7234aebbdc75095d6c26ccd41426c76389597d4d
Anyanwu Ugochukwu <lennyyhugoh@gmail.com>
2025-10-12 23:13:40 +0100

    added ui

--- src/main/java/com/darc/shortlink/controller/HomeController.java
+++ src/main/java/com/darc/shortlink/controller/HomeController.java
@@ -1,18 +1,20 @@
 package com.darc.shortlink.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String home() {
-        return "index.html";
+    public String home(Model model) {
+        model.addAttribute("title", "URL Shortener");
+        return "index";
     }
 
     @GetMapping("/about")
     public String about() {
-        return "about.html";
+        return "about";
     }
 }
